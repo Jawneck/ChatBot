00ac129148e66e579458b477c518df2f39b502e7
Danielis Joniskis <g00333859@gmit.ie>
2017-11-13 19:56:01 +0000

    using a regular expression

--- eliza.go
+++ eliza.go
@@ -4,29 +4,29 @@ package main
 import (
 	"fmt"
 	"net/http"
-//	"regexp"
+	"regexp"
 	"math/rand"
 	"time"
 )
 
-func userinputhandler(w http.ResponseWriter, r *http.Request) {
+func userinputhandler(w http.ResponseWriter, r *http.Request){
+
+	userinput :=  r.URL.Query().Get("value")	
+	if matched, _:= regexp.MatchString(`(?i).*\bhello\b.*`, userinput);matched{
+		fmt.Fprintf(w, "Hello, how are you today?")
+		return 
+	}
 
 	strings := []string{
 		"I'm not sure what you are talking about.",
 		"Could you elaborate on that",
 		"I think you are missing the point",
 		}
-		rand.Seed(time.Now().UnixNano())
+		rand.Seed(time.Now().UnixNano())		
 		response := strings[rand.Intn(len(strings))]
-
-	userinput :=  r.URL.Query().Get("value")
-	if(userinput == "Hello" || "hello"){
-	fmt.Fprintf(w, "%s there senor",userinput)
-	}else{
 		fmt.Fprintf(w, response)
-	}
-
 }
+
 func main(){
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/", fs)
